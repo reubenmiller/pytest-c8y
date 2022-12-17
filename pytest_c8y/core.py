c988f758f2a7cf93877770cb2418a202f38ef7a8
Reuben Miller <reuben.d.miller@gmail.com>
2022-12-17 20:31:01 +0100

    fix: core imports

--- pytest_c8y/core.py
+++ pytest_c8y/core.py
@@ -1,3 +1,12 @@
 # Export core package under the pytest_c8y package to
 # make imports easier
-from c8y_test_core import *
+from c8y_test_core import __version__
+from c8y_test_core import compare
+from c8y_test_core import context
+from c8y_test_core import device_management
+from c8y_test_core import errors
+from c8y_test_core import models
+from c8y_test_core import retry
+from c8y_test_core import task
+from c8y_test_core import timer
+from c8y_test_core import utils
